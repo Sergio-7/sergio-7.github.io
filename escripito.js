7f8a46a27afbf25ba5b06ab3a9f9d745d76d85fc
Sergio-7 <65739848+Sergio-7@users.noreply.github.com>
2020-05-22 17:17:13 +0200

    Subido por primera vez

---
+++ escripito.js
@@ -0,0 +1,6 @@
+var pregunta = confirm("Bienvenidos a Viva Viajes, este es el apartado de las reclamaciones. Usted quiere realizar alguna reclamación?");
+if (pregunta == true){
+	alert("Perfecto, rellene los siguientes datos que le aparecerán en pantalla y su reclamación será tramitada. ");
+} else {
+	alert("Muchas gracias y disfrute de nuestros servicios.");
+}
